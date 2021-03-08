1efcf0d62e6a5226a1fac4bec0c31e7d59b7aa35
Lisikm <lisikm@gmail.com>
2021-03-08 11:28:32 +0100

    Models

--- eMenu/models.py
+++ eMenu/models.py
@@ -1,3 +1,88 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+CATEGORY = (
+    ("American", "American"),
+    ("Italian", "Italian"),
+    ("Vietnamese", "Vietnamese"),
+    ("Indian", "Indian"),
+    ("Polish", "Polish"),
+    ("Chinese", "Chinese"),
+    ("Japanese", "Japanese"),
+    ("Mexican", "Mexican")
+)
+
+DOTW = (
+    (1, "Monday"),
+    (2, "Tuesday"),
+    (3, "Wednesday"),
+    (4, "Thursday"),
+    (5, "Friday"),
+    (6, "Saturday"),
+    (7, "Sunday")
+)
+
+HOURS = (
+    (1, "01:00"),
+    (2, "02:00"),
+    (3, "03:00"),
+    (4, "04:00"),
+    (5, "05:00"),
+    (6, "06:00"),
+    (7, "07:00"),
+    (8, "08:00"),
+    (9, "09:00"),
+    (10, "10:00"),
+    (11, "11:00"),
+    (12, "12:00"),
+    (13, "13:00"),
+    (14, "14:00"),
+    (15, "15:00"),
+    (16, "16:00"),
+    (17, "17:00"),
+    (18, "18:00"),
+    (19, "19:00"),
+    (20, "20:00"),
+    (21, "21:00"),
+    (22, "22:00"),
+    (23, "23:00"),
+    (24, "00:00")
+)
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.CharField(max_length=255)
+    category = models.CharField(choices=CATEGORY, max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class OpeningHours(models.Model):
+    day_of_the_week = models.IntegerField(choices=DOTW)
+    from_hour = models.IntegerField(choices=HOURS)
+    to_hour = models.IntegerField(choices=HOURS)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('day_of_the_week', 'restaurant')
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.CharField(max_length=255)
+    add_date = models.DateField(auto_now_add=True)
+    mod_date = models.DateField(auto_now_add=True)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    price = models.IntegerField()
+    preparation_time = models.IntegerField()
+    add_date = models.DateField(auto_now_add=True)
+    mod_date = models.DateField(auto_now_add=True)
+    is_wegetarian = models.BooleanField()
+    menu = models.ManyToManyField(Menu)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- eReservation/models.py
+++ eReservation/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from eMenu.models import Restaurant, HOURS
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Table(models.Model):
+    description = models.CharField(max_length=255)
+    num_of_chairs = models.IntegerField()
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+
+class Reservation(models.Model):
+    from_hour = models.IntegerField(choices=HOURS)
+    to_hour = models.IntegerField(choices=HOURS)
+    date = models.DateField()
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=255, null=True)
